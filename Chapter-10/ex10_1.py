6d85551abc2be6b948eafbd225eda6e0db3aafc2
HalnyGit <haliniakm@gmail.com>
2018-06-10 16:26:57 +0200

    lists

--- Chapter-10/ex10_1.py
+++ Chapter-10/ex10_1.py
@@ -2,8 +2,9 @@ def nested_sum(t):
 	"""Takes the nested list of integers and add update
 		the elements from all of the nested list
 		
-		t - list with nested list containing integers or floats
-		return - sum of all nested list
+		t: list with nested list containing integers or floats
+		
+		return: sum of all nested list
 	"""
 	collector = 0
 	for item in t:
