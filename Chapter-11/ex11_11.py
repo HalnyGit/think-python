2fcba72d0962955cf95ecf01c0e4a0ba2f721a1b
HalnyGit <haliniakm@gmail.com>
2018-08-11 15:18:42 +0200

    homophones

--- Chapter-11/ex11_11.py
+++ Chapter-11/ex11_11.py
@@ -1,9 +1,9 @@
-def read_dict(input_file):
+def build_dict(input_file):
 	"""Reads from a file and builds a dictionary that maps from
-	each word to astring that represents its pronuciation.
+	each word to a string that represents its pronuciation.
 	
 	input_file: file
-	returns: map from word to its pronunciation
+	returns: map from word to its pronunciationwor
 	"""
 	t = []
 	pronuciation_dict = dict()
@@ -12,44 +12,61 @@ def read_dict(input_file):
 	for line in fin:
 		#skip over the comments
 		if line[0] == '#': continue
-		
 		t.append(line.strip().split('  '))
-	
+
 	for elem in t:
-		pronuciation_dict[elem[0]] = elem[1]
+		if len(elem) > 1:
+			pronuciation_dict[elem[0]] = elem[1]
 	
 	return pronuciation_dict
-		
-infile = 'htest.txt'
-print read_dict(infile)
 
-def get_homophones(d):
-	"""Looks for homophones of five letter words with the following property:
+
+def is_homophone(word1, word2, pron_dict):
+	"""Chcecks if two words are homophones"
+	word1 - first word
+	word2 - second word
+	pron_dict - dictonary that maps strings/words to a string that represents its pronuciation
+	returns: True if two words are homophones or False if they are not
+	"""
+	
+	if pron_dict[word1] == pron_dict[word2]:
+		return True
+	return False
+	
+
+def get_homophones(pron_dict):
+	"""Looks for homophone words with the following property:
 	When you remove the first letter, the remaining letters form a homophone of the original word, that is a word
 	that sounds exactly the same. Replace the first letter, that is, put it back and remove
 	the second letter and the result is yet another homophone of the original word.
 	
 	d = dictionary that maps words to its pronuciation
-	returns: homophones
+	returns: list of homophones
 	"""
+	t = []
+	for word in pron_dict:
+		word2 = word[1:]
+		word3 = word[0] + word[2:]
+		check1 = False
+		check2 = False
+		if word2 in pron_dict:
+			check1 = is_homophone(word, word2, pron_dict)
+		if check1 == True and word3 in pron_dict:
+			check2 = is_homophone(word, word3, pron_dict)
+		if check2:
+			t.append(word)
 	
-	for k in d:
-		#looks for 5 letter words
-		if ')' in k: #includes words with secondary pronounciation
-			n = len(k[:-3])
-		else:
-			n = len(k)
+	return t
+
+	
+infile = 'c06d.txt'
+pron_dictonary = build_dict(infile)
+print get_homophones(pron_dictonary)
 		
-		#looks for 4 letter words after removing the first letter
-		if n == 5:
-			if ')' in k:
-				four_letter_word = k[1:-3]
-			else:
-				four_letter_word = k[1:]
-				
-			if four_letter_word in d:
-				if d[k] == d[four_letter_word]:
-					next_word
-					
+			
+		
+			
+			
+
 					
 					
