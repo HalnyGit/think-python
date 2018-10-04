a91438774d565219303761ffee65ae4086bcb930
HalnyGit <haliniakm@gmail.com>
2018-10-04 22:08:30 +0200

    text analysis

--- Chapter-13/ex13_1.py
+++ Chapter-13/ex13_1.py
@@ -1,23 +1,47 @@
 def read_words(infile):
-	
+	"""Creates the list of words from the file
+	infile: text file
+	return: list
+	"""	
 	import string
-	punctu = string.punctuation
+	p = string.punctuation
 	
 	fin = open(infile)
 	lines = fin.readlines()
 	words = []
-	for line in lines:
+	for line in lines[25:]: #skipping over the header information
 		t = line.split()
 		for word in t:
-			clean_word = word.strip(punctu).lower()
+			clean_word = word.strip(p).lower()
 			words.append(clean_word)
+	words.remove('')
 	return words
 
+def make_word_dict(words):
+	"""Creates dictonary of string from the word list with words as keys
+	and print the the number of times the words has been used as values
+	words: list of words
+	return: dictonary {word:number of occurrence}
+	"""
+	num_of_words = len(words)
+	print 'total number of words in text:', num_of_words
+	word_dict = dict()
+	for word in words:
+		word_dict[word] = word_dict.get(word, 0) + 1
+	return word_dict
+	
+def print_word_occur(word_dict, n):
+	"""Print out n words with the highest number of occurrences in the text
+	word_dict: dictonary {word:number of occurrence} 
+	"""
+	t = []
+	for word, freq in word_dict.iteritems():
+		t.append((freq, word))
+	t.sort(reverse=True) #DSU to sort by number of word occurrence	
+	for freq, word in t[:n]:
+		print word, freq
+	
 
-		
-		
-		
-
-
-print read_words('test.txt')
-
+words = read_words('dracula.txt')
+wd = make_word_dict(words)
+print_word_occur(wd, 100)
