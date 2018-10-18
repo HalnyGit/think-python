b50b1b56607dbe7658509c9feb9af6180c0f4242
HalnyGit <haliniakm@gmail.com>
2018-10-18 22:17:20 +0200

    os

---
+++ Chapter-14/ex14_1.py
@@ -0,0 +1,20 @@
+import os
+
+def walk(dirname):
+	for name in os.listdir(dirname):
+		path = os.path.join(dirname, name)
+		if os.path.isfile(path):
+			print path
+		else:
+			walk(path)
+
+			
+def walk2(dirname):
+	for root, dirs, files in os.walk(dirname):
+		for filename in files:
+			print os.path.join(root, filename)
+
+
+some_path = 'C:\Users\Public\Documents\IT\ThinkPython'
+walk2(some_path)
+
