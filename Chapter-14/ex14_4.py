1ff880ab46e4b366b034a85fff9a8491a77208d7
HalnyGit <haliniakm@gmail.com>
2018-10-30 22:00:44 +0100

    find duplicate files

--- Chapter-14/ex14_4.py
+++ Chapter-14/ex14_4.py
@@ -8,17 +8,29 @@ def find_mp3(dirname):
 				res.append(os.path.join(root, filename))
 	return res
 
-def check_file_sum(file_list):
+def get_file_sum(filename):
+	cmd = 'CertUtil -hashfile ' + filename + ' MD5'
+	fp = os.popen(cmd)
+	res = fp.readlines()
+	fp.close()
+	return res[1]
+
+def get_duplicates(file_list):
+	sum_file_map = dict()
+	duplicates = []
 	for filename in file_list:
-		cmd = 'CertUtil -hashfile ' + filename + ' MD5'
-		fp = os.popen(cmd)
-		res = fp.read()
-		print res
-		fp.close()
-
+		sum = get_file_sum(filename)
+		sum_file_map.setdefault(sum, []).append(filename)
+	for sum in sum_file_map:
+		if len(sum_file_map[sum]) > 1:
+			duplicates.append((sum_file_map[sum]))
+	return duplicates
+	
+		
 file_list = find_mp3('C:\Users\Komp\Music')
-check_file_sum(file_list)
-
+duplicates = get_duplicates(file_list)
+for i in duplicates:
+	print i
 
 
 
